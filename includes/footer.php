987d0d7ba57222203c4ef152c764eac490d00a78
SandeepaMapa <gayansandeepam88@gmail.com>
2022-11-08 23:41:11 +0530

    s

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <div class="footer-section">
 						<div class="container">
 							<div class="footer-top">
-								<p>Hotel Booking Management Sytem @ 2020 </p>
+								<p>Developed for River's Edge Madape by Group 14 </p>
 							</div>
 						
 				</div>
