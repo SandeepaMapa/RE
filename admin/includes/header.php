842a0c43db43db6a2b89e66893df9aa465cdb80d
thihansas <thihansaaa@gmail.com>
2022-11-09 03:35:53 +0530

    .

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -23,7 +23,7 @@
                             <div class="header">
                                 <div class="head-t">
                                     <div class="logo">
-                                        <a href="dashboard.php" style="font-size: 20px;color: red">Hotel Booking Management System </a>
+                                        <a href="dashboard.php" style="font-size: 20px;color: blue">Rivers edge  </a>
                                     </div>
                                         <!-- start header_right -->
                                     
